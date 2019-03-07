b7e8329ee4f12d9616e048a6b2a40f99d9f060a1
Aly Richardson <alyraptor101@gmail.com>
2019-03-06 22:37:29 -0600

    Created inital zoom transforms

--- Assets/Scripts/Interface/CameraController.cs
+++ Assets/Scripts/Interface/CameraController.cs
@@ -5,9 +5,21 @@ public class CameraController : MonoBehaviour {
 
 	public Transform target;
 	public float smoothTime = 0.15f;
+	public float zoomMod;
 
-	private float cameraHeight = 3f;
+    private Camera cam;
+
+    private float zoomStartTime;
+	private float cameraHeight = 2.3f;
 	private Vector3 velocity = Vector3.zero;
+
+	private float zoom = 1f;
+	private float currentZoom;
+
+	void Awake() {
+        cam = gameObject.GetComponent<Camera>();
+		currentZoom = cam.orthographicSize;
+	}
 
 	void Update () {
 		if (target != null) {
@@ -16,6 +28,16 @@ public class CameraController : MonoBehaviour {
 			goalPos.x = target.position.x - 1f;
 			goalPos.z = target.position.z - 1f;
 			transform.position = Vector3.SmoothDamp (transform.position, goalPos, ref velocity, smoothTime);
+
+            zoomStartTime = Time.time;
+            float fracZoom = (Time.time - zoomStartTime) / smoothTime;
+            if (fracZoom <= 1) {
+                cam.orthographicSize = Mathf.Lerp(currentZoom, zoom, fracZoom);
+            }
 		}
+	}
+
+	public void Zoom (float zoomChange) {
+		zoom = zoom + (zoomChange / zoomMod);
 	}
 }
