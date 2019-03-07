6bfeaecdc6467384124916f2d1a79708ae8b4505
Aly Richardson <alyraptor101@gmail.com>
2019-03-07 09:12:26 -0600

    Finished camera zoom logic

--- Assets/Scripts/Interface/CameraController.cs
+++ Assets/Scripts/Interface/CameraController.cs
@@ -5,7 +5,11 @@ public class CameraController : MonoBehaviour {
 
 	public Transform target;
 	public float smoothTime = 0.15f;
-	public float zoomMod;
+    public float zoomDuration = 0.15f;
+	public float zoomMod = 2f;
+	public float minZoom = 2f;
+	public float maxZoom = 20f;
+	public float zoomDefault = 3.5f;
 
     private Camera cam;
 
@@ -13,12 +17,14 @@ public class CameraController : MonoBehaviour {
 	private float cameraHeight = 2.3f;
 	private Vector3 velocity = Vector3.zero;
 
-	private float zoom = 1f;
-	private float currentZoom;
+	private float zoomTarget;
+	private float zoom;
+	private bool isZooming;
+
 
 	void Awake() {
         cam = gameObject.GetComponent<Camera>();
-		currentZoom = cam.orthographicSize;
+		zoom = zoomTarget = cam.orthographicSize = zoomDefault;
 	}
 
 	void Update () {
@@ -29,15 +35,31 @@ public class CameraController : MonoBehaviour {
 			goalPos.z = target.position.z - 1f;
 			transform.position = Vector3.SmoothDamp (transform.position, goalPos, ref velocity, smoothTime);
 
-            zoomStartTime = Time.time;
-            float fracZoom = (Time.time - zoomStartTime) / smoothTime;
-            if (fracZoom <= 1) {
-                cam.orthographicSize = Mathf.Lerp(currentZoom, zoom, fracZoom);
-            }
+            if(isZooming) {
+				float fracZoom = (Time.time - zoomStartTime) / zoomDuration;
+				if (fracZoom <= 1) {
+					cam.orthographicSize = Mathf.Lerp(zoom, zoomTarget, fracZoom);
+				} else {
+					cam.orthographicSize = zoomTarget;
+					isZooming = false;
+				}
+			}
 		}
 	}
 
 	public void Zoom (float zoomChange) {
-		zoom = zoom + (zoomChange / zoomMod);
+
+        zoom = cam.orthographicSize;
+
+		// Invert change in Zoom to account for ortho size being on a reverse scale
+		zoomChange = -zoomChange;
+		zoomTarget = zoomTarget + (zoomChange / zoomMod);
+
+		// If zoom is close to default, snap it to the default.
+		zoomTarget = Mathf.Abs(zoomTarget - zoomDefault) <= 0.25f ? zoomDefault : zoomTarget;
+		zoomTarget = zoomTarget < minZoom ? minZoom : zoomTarget > maxZoom ? maxZoom : zoomTarget;
+
+        zoomStartTime = Time.time;
+		isZooming = true;
 	}
 }
