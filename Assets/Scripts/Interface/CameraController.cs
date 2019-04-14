4a0ace1d7445d22d817186110ea722430cec68a8
Aly Richardson <alyraptor101@gmail.com>
2019-04-14 18:54:14 -0500

    Converted to AngleAxis -- probably not going to use, but archiving work just in case

--- Assets/Scripts/Interface/CameraController.cs
+++ Assets/Scripts/Interface/CameraController.cs
@@ -4,86 +4,93 @@ using UnityEngine;
 namespace TowerDefense {
     public class CameraController : MonoBehaviour {
 
-		public float smoothTime = 0.15f; // How much to smooth camera movement (in seconds)
-		public float zoomDuration = 0.15f; // How long a zoom lasts (in seconds)
-		public float zoomMod = 1.5f;  // Dampen user input
-		public float rotationDuration = 5f; // How long a rotation lasts (in seconds)
+        public float smoothTime = 0.15f; // How much to smooth camera movement (in seconds)
+        public float zoomDuration = 0.15f; // How long a zoom lasts (in seconds)
+        public float zoomMod = 1.5f;  // Dampen user input
+        public float rotationDuration = 0.25f; // How long a rotation lasts (in seconds)
 
-		private Camera cam;
-		private GameObject camParent;
-		private Transform target; // The camera will follow this target
+        private Camera cam;
+        private GameObject camParent;
+        private Transform target; // The camera will follow this target
 
-		private float minZoom = 2f; // Minimum the player can zoom (in Ortho camera size)
-		private float maxZoom = 15f; // Maximum the player can zoom (in Ortho camera size)
-		private float zoomDefault = 5f; // Starting Zoom level (in Ortho camera size)
-		private float cameraHeight = 1.3f;
-		private Vector3 velocity = Vector3.zero; // Starting velocity for SmoothDamp
+        private float minZoom = 2f; // Minimum the player can zoom (in Ortho camera size)
+        private float maxZoom = 15f; // Maximum the player can zoom (in Ortho camera size)
+        private float zoomDefault = 5f; // Starting Zoom level (in Ortho camera size)
+        private float cameraHeight = 1.3f;
+        private Vector3 velocity = Vector3.zero; // Starting velocity for SmoothDamp
 
-		private float zoom; // Start zoom for method
-		private float zoomTarget; // End zoom for method
-		private float zoomStartTime; // Reference of time when method starts
-		private bool isZooming;
+        private float zoom; // Start zoom for method
+        private float zoomTarget; // End zoom for method
+        private float zoomStartTime; // Reference of time when method starts
+        private bool isZooming;
 
-		private Quaternion rotation; // Start rotation for method
-		private Quaternion rotationTarget; // End rotation for method
-		private float rotationStartTime; // Reference of time when method starts
-		private bool isRotating;
+        private Quaternion rotation; // Start rotation for method
+        private Quaternion rotationTarget; // End rotation for method
+        private float targetAngle; // Target angle of rotation
+        private float rotationStartTime; // Reference of time when method starts
+        private bool isRotating;
 
-		void Awake() {
-			cam = gameObject.GetComponent<Camera>();
-			camParent = cam.transform.parent.gameObject;
-			zoom = zoomTarget = cam.orthographicSize = zoomDefault;
-			target = GameObject.FindWithTag("Player").transform;
-		}
+        void Awake() {
+            cam = gameObject.GetComponent<Camera>();
+            camParent = cam.transform.parent.gameObject;
+            zoom = zoomTarget = cam.orthographicSize = zoomDefault;
+            target = GameObject.FindWithTag("Player").transform;
+        }
 
-		void Update () {
-			if (target != null) {
-				Vector3 goalPos = new Vector3(target.position.x, cameraHeight, target.position.z);
-				camParent.transform.position = Vector3.SmoothDamp(camParent.transform.position, goalPos, ref velocity, smoothTime);
+        void Update() {
+            if (target != null) {
+                Vector3 goalPos = new Vector3(target.position.x, cameraHeight, target.position.z);
+                camParent.transform.position = Vector3.SmoothDamp(camParent.transform.position, goalPos, ref velocity, smoothTime);
 
-				if(isZooming) {
-					float fracZoom = (Time.time - zoomStartTime) / zoomDuration;
-					if (fracZoom <= 1) {
-						cam.orthographicSize = Mathf.Lerp(zoom, zoomTarget, fracZoom);
-					} else {
-						cam.orthographicSize = zoomTarget;
-						isZooming = false;
-					}
-				}
-				if(isRotating) {
+                if (isZooming) {
+                    float fracZoom = (Time.time - zoomStartTime) / zoomDuration;
+                    if (fracZoom <= 1) {
+                        cam.orthographicSize = Mathf.Lerp(zoom, zoomTarget, fracZoom);
+                    } else {
+                        cam.orthographicSize = zoomTarget;
+                        isZooming = false;
+                    }
+                }
+                if (isRotating) {
                     float fracRotation = (Time.time - rotationStartTime) / rotationDuration;
                     if (fracRotation <= 1) {
                         camParent.transform.rotation = Quaternion.Slerp(rotation, rotationTarget, fracRotation);
                     } else {
+                        if (targetAngle >= 360) {
+                            targetAngle -= 360;
+                            rotationTarget = Quaternion.AngleAxis(targetAngle, Vector3.up);
+                        }
                         camParent.transform.rotation = rotationTarget;
                         isRotating = false;
                     }
-				}
-			}
-		}
+                }
+            }
+        }
 
-		public void Zoom (float zoomChange) { // Ease-in/Ease-out Zoom
+        public void Zoom(float zoomChange) { // Ease-in/Ease-out Zoom
 
-			isZooming = true;
-			zoomStartTime = Time.time;
-			zoom = cam.orthographicSize;
+            zoomStartTime = Time.time;
+            zoom = cam.orthographicSize;
 
-			// Invert change in Zoom to account for ortho size being on a reverse scale
-			zoomChange = -zoomChange;
-			zoomTarget = zoomTarget + (zoomChange / zoomMod);
+            // Invert change in Zoom to account for ortho size being on a reverse scale
+            zoomChange = -zoomChange;
+            zoomTarget = zoomTarget + (zoomChange / zoomMod);
 
-			// If zoom is close to default, snap it to the default.
-			zoomTarget = Mathf.Abs(zoomTarget - zoomDefault) <= 0.25f ? zoomDefault : zoomTarget;
-			zoomTarget = zoomTarget < minZoom ? minZoom : zoomTarget > maxZoom ? maxZoom : zoomTarget;
-		}
+            // If zoom is close to default, snap it to the default.
+            zoomTarget = Mathf.Abs(zoomTarget - zoomDefault) <= 0.25f ? zoomDefault : zoomTarget;
+            zoomTarget = zoomTarget < minZoom ? minZoom : zoomTarget > maxZoom ? maxZoom : zoomTarget;
 
-		public void Rotate (float rotationChange) {
+            isZooming = true;
+        }
+
+        public void Rotate(float rotationChange) {
+
+            rotation = camParent.transform.rotation;
+            targetAngle += rotationChange;
+            rotationStartTime = Time.time;
+            rotationTarget = Quaternion.AngleAxis(targetAngle, Vector3.up);
 
             isRotating = true;
-            rotationStartTime = Time.time;
-            rotation = camParent.transform.rotation;
-
-            rotationTarget = rotation * Quaternion.Euler(new Vector3(0, rotationChange, 0));
-		}
-	}
+        }
+    }
 }
